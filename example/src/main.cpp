c250395b73fb5e2fd9274a66a99b5da6e3e1c5c1
Zach Lambert <zachalambert@gmail.com>
2022-09-25 22:50:04 +0100

    Minor example changes

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -9,7 +9,6 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
-#define PRINTF_ALIAS_STANDARD_FUNCTION_NAMES 0
 #include <nanoprintf.h>
 
 
@@ -25,7 +24,7 @@ void gpio_setup(void)
         GPIOA,
         GPIO_MODE_OUTPUT_2_MHZ,
 		GPIO_CNF_OUTPUT_PUSHPULL,
-        GPIO3 | GPIO4 | GPIO5
+        GPIO4 | GPIO5
     );
 }
 
@@ -61,18 +60,18 @@ void main_task(void* args)
     char message[32] = "hello\n";
     int i = 0;
     int delay_ms = 1;
-    int ticks_per_write = 10;
+    int ticks_per_write = 50;
     while (true) {
 		usbd_poll(usbd_dev);
         if (i == 0) {
             npf_snprintf(message, sizeof(message), "Time: %f\n", time);
-            time += ((float)delay_ms * ticks_per_write) * 1e-3;;
+            time += ((float)delay_ms * ticks_per_write) * 1e-3;
             usbd_ep_write_packet(usbd_dev, 0x82, message, strnlen(message, sizeof(message)));
-            gpio_set(GPIOA, GPIO3);
+            gpio_set(GPIOA, GPIO4);
         }
         i++;
         if (i == ticks_per_write / 2) {
-            gpio_clear(GPIOA, GPIO3);
+            gpio_clear(GPIOA, GPIO4);
         }
         if (i == ticks_per_write) {
             i = 0;
